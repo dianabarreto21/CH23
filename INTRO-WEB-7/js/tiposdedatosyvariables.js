11616122df20d7501df0deba4aad80a65962e51a
Diana <dianabarretucha@gmail.com>
2023-02-09 20:34:15 -0600

    actividades de INTRO-WEB-6, INTRO-WEB-7 9 feb 23

---
+++ INTRO-WEB-7/js/tiposdedatosyvariables.js
@@ -0,0 +1,23 @@
+var nombre = "Diana";
+var edad= 27;
+var tengoMascotas= true;
+var tengoTrabajo= false;
+var tengoSueldo= null;
+
+
+let nombre= "diana";
+let edad= 27;
+let tengoMascota= true;
+let tengoTrabajo= false;
+let tengoSueldo; null;
+
+/*Ejercicios */
+
+let horasTrabajadas= prompt("¿Cuantas horas trabajaste?");
+let sueldoPorHora= prompt("Cuanto te pagan por hora");
+
+let n1=(3+5);
+let n2= (2*5);
+let n2= (n1/n2);
+
+function operacion
